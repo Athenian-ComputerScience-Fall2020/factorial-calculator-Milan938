2c5db6a7345c0963dad0b99900167b452226442e
Milan938 <23mboga@athenian.org>
2020-09-15 00:04:15 +0000

    Finished

--- my_code.py
+++ my_code.py
@@ -1,11 +1,15 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
-x = 7
-def factorial_calc(x):   #you may choose the name of the parameter
+#  None
+
+def factorial_calc(x): 
+    total = 1
     for x in range(1, x+1):
-        x = x + 1
-    print(x)
+        total = total * x
+        
+    return total
 
 # be sure to return the factorial
 
-
+if __name__=='__main__':
+    x = int(input("Enter a number: "))
+    print(factorial_calc(x))
